4108e89127df54965e7ad5e24ca371efc55a4d43
Yuriy Koziy <yukoziy@gmail.com>
2019-09-28 00:21:22 -0400

    Moved Header component to the containers folder, because it is now a component that is connected to the Redux store (container).

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "../assets/stylesheets/App.css";
 
-import Header from "./Header.js";
+import Header from "../containers/Header.js";
 
 class App extends Component {
   render() {
