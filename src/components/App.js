794233fe707f382c55026f5226e2be2f58444211
Yuriy Koziy <yukoziy@gmail.com>
2019-09-27 21:35:44 -0400

    Modified App.js, to a class component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { Component } from "react";
 import '../assets/stylesheets/App.css';
 
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
 }
 
 export default App;
