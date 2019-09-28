4dd279e32f8681d3c456a37b8e0625affcee5625
Yuriy Koziy <yukoziy@gmail.com>
2019-09-27 21:43:25 -0400

    Added a component, Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+const Header = (props) => {
+    return(
+        <div id="head">
+            <div id="head-text">
+                <h1>Drum Machine</h1>
+            </div>
+            <div id="pwr-btn-container">
+                <button id="pwr-btn"
+                    className={props.powerState ? "on" : null}
+                    onClick={props.switchPower}
+                >
+                    <i className="far fa-dot-circle"/>
+                </button>
+            </div>
+        </div>
+    );
+}
