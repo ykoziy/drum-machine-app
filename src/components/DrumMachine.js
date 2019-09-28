4bcea0ffd60204ef6af5a56079cd1192014d3ebc
Yuriy Koziy <yukoziy@gmail.com>
2019-09-28 00:45:24 -0400

    DrumMachine component now uses className, added more css styling for the app.

--- src/components/DrumMachine.js
+++ src/components/DrumMachine.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const DrumMachine = (props) => {
   return(
-    <div id="drum-machine">
+    <div className="drum-machine">
         {props.left}
         {props.right}
     </div>
