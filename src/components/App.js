e13140111b30f989f41192e44ee4a671b05f553c
Yuriy Koziy <yukoziy@gmail.com>
2019-09-28 00:47:06 -0400

    Added DrumMachine component to the App component.

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,25 @@ import React, { Component } from "react";
 import "../assets/stylesheets/App.css";
 
 import Header from "../containers/Header.js";
+import DrumMachine from "./DrumMachine.js"
 
 class App extends Component {
   render() {
     return (
       <div className="drum-container">
         <Header/>
+        <DrumMachine
+            left={
+                <div className="button-panel">
+                Buttons
+                </div>
+            }
+            right={
+                <div>
+                Right
+                </div>
+            }
+        />
       </div>
     );
   }
