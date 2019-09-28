27f3893645b5ed0e9191bc637e97a634e16a9e1b
Yuriy Koziy <yukoziy@gmail.com>
2019-09-28 00:55:11 -0400

    Converted div's to use classes instead of id's

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -4,13 +4,12 @@ import { switchPower } from '../actions'
 
 const Header = ({power, switchPower}) => {
     return(
-        <div id="head">
-            <div id="head-text">
+        <div className="head">
+            <div className="head-text">
                 <h1>Drum Machine</h1>
             </div>
-            <div id="pwr-btn-container">
-                <button id="pwr-btn"
-                    className={power ? "on" : null}
+            <div className="pwr-btn-container">
+                <button className={`pwr-btn ${power ? "on" : null}`}
                     onClick={switchPower}
                 >
                     <i className="far fa-dot-circle"/>
@@ -32,3 +31,4 @@ export default connect(
     mapStateToProps,
     mapDispatchToProps
 )(Header);
+
