affecdfbe37334193ac0f9a02040b0e726322395
Yuriy Koziy <yukoziy@gmail.com>
2019-09-28 00:15:45 -0400

    Added Header component to the app, removed DrumMachine import for now.

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,12 @@ import React, { Component } from "react";
 import "../assets/stylesheets/App.css";
 
 import Header from "./Header.js";
-import DrumMachine from "./DrumMachine.js"
 
 class App extends Component {
   render() {
     return (
       <div className="drum-container">
+        <Header/>
       </div>
     );
   }
