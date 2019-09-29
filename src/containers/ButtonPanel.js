c25b8f46349b77c5ffd838bafca26b8401a3b984
Yuriy Koziy <yukoziy@gmail.com>
2019-09-29 18:03:07 -0400

    Added pressPad dispatch to the ButtonPanel container

--- src/containers/ButtonPanel.js
+++ src/containers/ButtonPanel.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { connect } from 'react-redux';
+import { pressPad } from '../actions'
 import { KIT_TITLES, DRUM_KIT_ONE, DRUM_KIT_TWO} from '../constants/drumData.js';
 import Button from '../components/Button.js';
 
-const ButtonPanel = ({power, volumeVal, bankName}) => {
+const ButtonPanel = ({power, volumeVal, bankName, pressPad}) => {
     let drumKit = (bankName === KIT_TITLES[0]) ? DRUM_KIT_ONE: DRUM_KIT_TWO;
-
     return(
         <div className="button-panel">
             {drumKit.map((i, idx) => (
                 <Button
                     key={"btn-"+idx} text={i.key} soundName={i.title}
                     url={i.url} volume={volumeVal} power={power}
+                    onClick={pressPad}
                 />
             ))}
         </div>
@@ -25,8 +26,7 @@ const mapStateToProps = (state) => ({
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  //adjustVolume: (event) => dispatch(adjustVolume(event.target.value))
-  //onDrumPadClick: (event) => dispatch(onDrumPadClick(event))
+  pressPad: (padName) => dispatch(pressPad(padName))
 });
 
 export default connect(
