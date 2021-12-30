f884a19479f8feafa47b830deac4c81981162c72
Yuriy Koziy <yukoziy@gmail.com>
2021-12-30 16:03:49 -0500

    Updates

---
+++ precache-manifest.9497316489a786628f98bde5daa4887b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc8303c0cb199d4e5d059cec392e9ef3",
+    "url": "/drum-machine-app/index.html"
+  },
+  {
+    "revision": "bdab46626a40c1cc6ccc",
+    "url": "/drum-machine-app/static/css/main.d75b6a92.chunk.css"
+  },
+  {
+    "revision": "2e5d3da15d845258776c",
+    "url": "/drum-machine-app/static/js/2.8f85ef24.chunk.js"
+  },
+  {
+    "revision": "029a7316550327a109a509edaaa5f28b",
+    "url": "/drum-machine-app/static/js/2.8f85ef24.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bdab46626a40c1cc6ccc",
+    "url": "/drum-machine-app/static/js/main.cdb706c3.chunk.js"
+  },
+  {
+    "revision": "e67e29a939565ed9c85a",
+    "url": "/drum-machine-app/static/js/runtime-main.bb3afadd.js"
+  }
+]);
